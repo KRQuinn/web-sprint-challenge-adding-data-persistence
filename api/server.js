ed259f3c9c39ebda4cd19cc810faa54f79e3a8da
KRQuinn <kr.quinn82@gmail.com>
2021-11-04 21:15:04 -0700

    setup project, filled out index, basic server ready to listen

--- api/server.js
+++ api/server.js
@@ -1,1 +1,22 @@
 // build your server here and require it from index.js
+
+const express = require('express')
+
+const server = express()
+
+server.use(express.json())
+
+
+
+server.use('*', (req, res, next) => {
+  next({ status: 404, message: 'This endpoint does not exist' })
+})
+
+//eslint-disable-next-line
+server.use((err, req, res, next) => {
+  res
+    .status(err.status || 500)
+    .json({ message: err.message, stack: err.stack || null })
+})
+
+module.exports = server
